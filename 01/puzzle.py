b81e03d65fca0c097b621486c83a379f6baa6140
Nick <nick.sologoub@outlook.com>
2022-12-03 12:21:00 -0500

    day 1

---
+++ 01/puzzle.py
@@ -0,0 +1,47 @@
+
+def puzzle1(lines):
+    max = 0
+    sum = 0
+
+    for line in lines:
+        if line.strip() == '':
+            sum = 0
+        else:    
+            num = int(line)
+            sum += num
+            if sum > max:
+                max = sum
+
+    return max
+
+def puzzle2(lines):
+    max1 = 0
+    max2 = 0
+    max3 = 0
+    sum = 0
+
+    sums = set()
+
+    for line in lines:
+        if line.strip() == '':
+            sums.add(sum)
+            sum = 0
+        else:    
+            num = int(line)
+            sum += num
+    
+    sums.add(sum)
+    # sort sums
+    sums = sorted(sums, reverse=True)
+
+    return sums[0] + sums[1] + sums[2]
+
+
+with open('/Users/nicksologoub/_src_/AdventOfCode2022/01/input.txt') as f:
+  lines = f.readlines()
+
+p1 = puzzle1(lines)
+p2 = puzzle2(lines)
+
+print(p1)
+print(p2)
